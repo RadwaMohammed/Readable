9b4dc5c473bf98d4b34d68a5fdfd0c3e9e3421db
radwa <mail2radwa@gmail.com>
2021-07-02 23:26:45 +0200

    feat: Add Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-        Readable
-      </div>
-    )
-  }
+function Header() {
+  return (
+    <Link to='/' className="header-link">
+      <FaHome className="home-icon" /> Readable
+    </Link>
+  )
 }
+export default Header;
