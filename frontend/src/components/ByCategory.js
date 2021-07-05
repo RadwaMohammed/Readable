c4ecdcb5a7af59615c46bde6d9d9cff134d3414c
radwa <mail2radwa@gmail.com>
2021-07-05 13:52:18 +0200

    feat: Build ByCategory component

--- frontend/src/components/ByCategory.js
+++ frontend/src/components/ByCategory.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { sortBy } from '../utils/helper';
+import Categories from './Categories';
+import PostsList from './PostsList';
+import AddPostBtn from './AddPostBtn';
+import SortBy from './SortBy';
 
-export default class ByCategory extends Component {
+class ByCategory extends Component {
+  state={
+    sortOption: 'timestamp-desc'
+  }
+  /**
+   * Update ths state with the sorte option
+   */
+  handleSorting = value => this.setState({sortOption: value});
+  
   render() {
+    const { sortOption } = this.state;
+    // Sort posts depending on user's option
+    const posts = sortBy(this.props.posts, sortOption);
     return (
-      <div>
-        ByCategory
-      </div>
+      <Fragment>
+        <Categories />
+        <AddPostBtn />
+        <SortBy handleSorting={this.handleSorting} />
+        <PostsList posts={posts} />
+      </Fragment>
     )
+
   }
 }
+
+/**
+ * The mapStateToProps function - get the state parts that ByCategory component needs
+ * @param {Object} state - The state of the store 
+ * @param {object} state.posts - The  posts slice of the state 
+ * @param {Object} props - The component's ownProps
+ * @returns {object} An object containing  posts {array} filtered by category 
+ */
+ const mapStateToProps = ({ posts }, props) => {
+  const { category } = props.match.params;
+  return {
+    posts: Object.values(posts).filter(post => post.category === category),
+  }
+  
+};
+export default connect(mapStateToProps)(ByCategory);
