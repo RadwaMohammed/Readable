4b6849fc421a8d7048029ed448b2709a63a729cf
radwa <mail2radwa@gmail.com>
2021-07-06 11:08:56 +0200

    feat: Handle adding comment

--- frontend/src/components/PostDetails.js
+++ frontend/src/components/PostDetails.js
@@ -1,12 +1,49 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { formatDate } from '../utils/helper';
+import { handleAddComment } from '../actions/comments';
 import CommentsList from './CommentsList';
-import { FiChevronUp, FiChevronDown, FiTrash2, FiEdit } from "react-icons/fi";
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { FaChevronUp, FaChevronDown, FaTrashAlt, FaEdit } from "react-icons/fa";
 
 
 class PostDetails extends Component {
+  state = {
+    commentAuthor: '',
+    comment: ''
+  };
+  /**
+   * Handle the change on the input text 
+   * in the add a commet form
+   * @param {e} e - event object 
+   */
+   handleChange = e => {
+    const { id, value } = e.target;
+    this.setState({
+      [id]: value
+    });
+  }
+  
+  /**
+   * Handle the submit to add new post
+   * @param {object} e - The event object 
+   */
+   handleSubmit = e => {
+    const { commentAuthor: author, comment: body } = this.state;
+    const { handleAddComment, postId: parentId } = this.props;
+    const comment = { author, body, parentId };
+    e.preventDefault();
+    handleAddComment(comment);
+    this.setState({
+      commentAuthor: '',
+      comment: ''
+    });
+  }
   render() {
+    const { commentAuthor, comment } = this.state;
     const { post, comments } = this.props;
     const {
       timestamp,
@@ -26,15 +63,55 @@ class PostDetails extends Component {
           <p>body: {body}</p>
           <p>author: {author}</p>
           <p>category: {category}</p>
-          <button><FiChevronUp /></button>
+          <button><FaChevronUp /></button>
           <p>vote: {voteScore}</p>
-          <button><FiChevronDown /></button>
+          <button><FaChevronDown /></button>
           <p>comment: {commentCount}</p>
-          <button><FiTrash2 /></button>
+          <button><FaTrashAlt /></button>
           <p></p>
-          <button><FiEdit /></button>
+          <button><FaEdit /></button>
           <hr />
         </div>
+        <Accordion>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              Add a comment
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <Form onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="commentAuthor">
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control 
+                      type="text" 
+                      placeholder="Enter Your Name" 
+                      value={commentAuthor}
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                
+                  <Form.Group controlId="comment">
+                    <Form.Label>Comment</Form.Label>
+                    <Form.Control 
+                      as="textarea" 
+                      rows={2}  
+                      placeholder="Type Your Comment"
+                      value={comment}
+                      onChange={this.handleChange}
+                    />
+                  </Form.Group>
+                  <Button 
+                    variant="primary" 
+                    type="submit"
+                    disabled={!commentAuthor.trim() || !comment.trim()}
+                  >
+                    Add a comment
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
         <CommentsList comments={comments}/>
       </Fragment>
     )
@@ -64,4 +141,13 @@ class PostDetails extends Component {
   }
 };
 
-export default connect(mapStateToProps)(PostDetails);
+/**
+ * The mapDispatchToProps as an object - dispatching actions to the store
+ * <handleAddPost> ction creator
+ */
+ const mapDispatchToProps = {
+  handleAddComment,
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetails);
